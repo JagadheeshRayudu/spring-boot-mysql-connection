cb0866489b183e0855aba5484614b8d8375a6773
Jagadheesh <jagadheeshrayudu@gmail.com>
2022-12-10 18:21:08 +0530

    Initial commit

---
+++ src/test/java/com/spring/boot/mysql/spring/boot/mysql/connection/SpringBootMysqlConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.mysql.spring.boot.mysql.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
